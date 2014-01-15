d943f7ba806dd43f8a92d6d2d796e000da453950
Hafiz <ibnukamy@gmail.com>
2014-01-15 22:13:54 +0800

    Implement displaying a list of relevant tickets. You can see them on projects, and view particular ticket.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,5 @@
 class Ticket < ActiveRecord::Base
+  belongs_to :project
   validates :title, presence: true
   validates :description, presence: true,
                           length: { minimum: 10 }
---
+++ spec/factories/ticket_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :ticket do
+    title "Example title"
+    description "An example ticket, nothing more"
+  end
+end
