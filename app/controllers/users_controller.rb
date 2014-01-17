b19b766ece1651333388daaf64ffa2948bdfdc59
Hafiz <ibnukamy@gmail.com>
2014-01-17 18:50:21 +0800

    Implement profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -7,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "You have signed up successfully."
       redirect_to projects_path
     else
@@ -17,12 +20,25 @@ class UsersController < ApplicationController
   def show
   end
 
-  private
-
-  def user_params
-    params.require(:user).permit(:name,
-                                 :password,
-                                 :password_confirmation)
+  def edit
   end
-end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile has been updated."
+      redirect_to @user
+    else
+      flash[:alert] = "Profile has not been updated."
+      render "edit"
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
