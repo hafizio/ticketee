e8eafcd52d0696c9d2983e65404002b99a7ee519
Hafiz <ibnukamy@gmail.com>
2014-01-25 06:27:31 +0800

    Make projects only visible to users with permission to see them

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :authorize_admin!, except: [:index, :show]
+  before_action :require_signin!, only: [:show]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -63,7 +64,12 @@ class ProjectsController < ApplicationController
   end
 
   def set_project
-    @project = Project.find(params[:id])
+    if current_user.admin?
+      @project = Project.find(params[:id])
+    else
+      @project = Project.viewable_by(current_user).find(params[:id])
+    end
+    
   rescue ActiveRecord::RecordNotFound
     flash[:alert] = "The project you were looking for could not be found."
     redirect_to projects_path
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe ProjectsController do
-  let(:user) { FactoryGirl.create(:user) } 
+  let(:user) { FactoryGirl.create(:user) }
   before do
     sign_in(user)
   end
@@ -15,19 +15,29 @@ describe ProjectsController do
     expect(flash[:alert]).to eql(message)
   end
 
-  context "standard user" do
+  context "standard users" do
     { new: :get,
       create: :post,
       edit: :get,
       update: :put,
       destroy: :delete }.each do |action, method|
-        
+
       it "cannot access the #{action} action" do
         sign_in(user)
+
         send(method, action, :id => FactoryGirl.create(:project))
+
         expect(response).to redirect_to(root_path)
         expect(flash[:alert]).to eql("You must be an admin to do that.")
       end
     end
+  end
+
+  it "cannot access the show action without permission" do
+    project = FactoryGirl.create(:project)
+    get :show, id: project.id
+
+    expect(response).to redirect_to(projects_path)
+    expect(flash[:alert]).to eql("The project you were looking for could not be found.")
   end
 end
