a958c4d6856a1a96896db758a0c2c10eb5a1ad8a
Hafiz <ibnukamy@gmail.com>
2014-05-07 12:04:15 +0800

    Users can now search by tag or state

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -4,19 +4,23 @@ feature "Searching" do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:project) { FactoryGirl.create(:project) }
   let!(:ticket_1) do
+  	state = State.create(name: "Open")
     FactoryGirl.create(:ticket,
             title: "Create projects",
             project: project,
             user: user,
-            tag_names: "iteration_1" )
+            tag_names: "iteration_1",
+            state: state )
   end
 
   let!(:ticket_2) do
+  	state = State.create(name: "Closed")
     FactoryGirl.create(:ticket,
             title: "Create users",
             project: project,
             user: user,
-            tag_names: "iteration_2" )
+            tag_names: "iteration_2",
+            state: state )
   end
 
   before do
@@ -33,6 +37,16 @@ feature "Searching" do
     click_button "Search"
 
     within("#tickets") do
+      expect(page).to have_content("Create projects")
+      expect(page).to_not have_content("Create users")
+    end
+  end
+
+  scenario "Finding bt state" do
+  	fill_in "Search", with: "state:Open"
+  	click_button "Search"
+
+ 		within("#tickets") do
       expect(page).to have_content("Create projects")
       expect(page).to_not have_content("Create users")
     end
